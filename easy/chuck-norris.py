ae0fa5c4e4d678536e299e30e419baa6872e2583
RomanBogatikov <ivaroman2000@gmail.com>
2020-04-25 10:39:32 -0400

    chuck norris easy final solution

--- easy/chuck-norris.py
+++ easy/chuck-norris.py
@@ -5,18 +5,25 @@ import math
 # the standard input according to the problem statement.
 
 message = input()
-# print(format(ord(message), 'b'))
-binary = ''.join(format(ord(char), 'b') for char in message)
-binaryString = str(binary)
-output = ''
-# print(binaryString)
+# function to convert binary string to 7-bit string
+def formatToSevenBits(binary):
+    string = str(binary)
+    while len(string) != 7:
+        string = '0' + string
+    return string
+
+# format each char as a 7-bit binary string and join them in a string
+binaryString = ''.join(formatToSevenBits(format(ord(char), 'b')) for char in message)
+
+# read first digit
 previousChar = binaryString[0]
 if (previousChar == '0'):
     result = '00 0'
 else:
     result = '0 0'
 
-for index, digit in enumerate([i for i in str(binary)]):
+# iterate over all other digits (except the first one)
+for index, digit in enumerate([i for i in binaryString]):
     if index != 0:
         if digit == previousChar:
             result += '0'
